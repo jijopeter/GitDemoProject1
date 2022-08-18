daf592e45db27219d946aacd0b6cf205b385f167
Admin <Admin@DESKTOP-C3T8P9A>
2022-08-18 17:46:42 +0530

    commit test2 to BranchX

---
+++ src/defaultpackage/Test3.java
@@ -0,0 +1,15 @@
+package defaultpackage;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		String join="yes";
+		if(join == "yes")
+			System.out.println("go to office");
+		else {
+			System.out.println("work from home");
+		}
+
+	}
+
+}
