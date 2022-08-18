0007ab3889f8411bc45fca6e489252593f920a6f
Admin <Admin@DESKTOP-C3T8P9A>
2022-08-18 14:47:25 +0530

    First commit...

---
+++ src/defaultpackage/Test.java
@@ -0,0 +1,15 @@
+package defaultpackage;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String join="yes";
+		if(join == "yes")
+			System.out.println("go to office");
+		else {
+			System.out.println("work from home");
+		}
+
+	}
+
+}
